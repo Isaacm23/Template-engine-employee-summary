ef8dafbf73b1afc24cdc27efc24d6b3033176831
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-06-10 14:55:01 -0700

    added questions, and index

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./employee");
+
+class Intern extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole() {
+    return "intern";
+  }
+}
+
+module.exports = Engineer;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole() {
+    return "Manager";
+  }
+}
+
+module.exports = Engineer;
